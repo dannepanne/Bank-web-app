896a835360f905a2d03a07f21ed28bbfa7b33f4d
Daniel Lenberg <danneeeee@hotmail.com>
2022-03-31 12:33:10 +0200

    Add project files.

---
+++ BankStartWeb/Pages/CustomerView/CustomerView.cshtml.cs
@@ -0,0 +1,34 @@
+using BankStartWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankStartWeb.Pages.CustomerView
+{
+    public class CustomerViewModel : PageModel
+    {
+        public int Id { get; set; }
+        public string Givenname { get; set; }
+        public string Surname { get; set; }
+        public string Streetaddress { get; set; }
+
+
+        private readonly ApplicationDbContext _context;
+
+        public CustomerViewModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet(int custId)
+        {
+
+            Customer currentCustomer = new Customer();
+            
+                currentCustomer = _context.Customers.First(x => x.Id == custId);
+                Id = currentCustomer.Id;
+                Givenname = currentCustomer.Givenname;
+                Surname = currentCustomer.Surname;
+                Streetaddress = currentCustomer.Streetaddress;
+                
+        }
+    }
+}
---
+++ BankStartWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using BankStartWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankStartWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public int Inputcustid { get; set; }
+
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly ILogger<IndexModel> _logger;
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+            _logger = logger;
+        }
+
+        public int AntalKunder { get; set; }
+        public decimal TotalSummaKonton { get; set; }
+        public int AntalKonton { get; set; }
+
+        public void OnGet()
+        {
+            AntalKunder = _applicationDbContext.Customers.Count();
+            AntalKonton = _applicationDbContext.Accounts.Count();
+            TotalSummaKonton = _applicationDbContext.Accounts.Sum(item => item.Balance);
+        }
+        public IActionResult OnPost()
+        {
+            int custId = Inputcustid;
+            return RedirectToPage("/CustomerView/CustomerView", new { custId = custId }) ;
+
+
+        }
+    }
+}
